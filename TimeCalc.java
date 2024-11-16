f0702e0faa43b1e18a74509bac30bcab0f7154bc
Liel Tal <liel.tal@post.runi.ac.il>
2024-11-16 23:37:13 +0200

    time calc

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,35 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        String time = args[0];
+        int minutesToAdd = Integer.parseInt(args[1]);
+
+        int ONE_HOUR_IN_MINUTES = 60;
+        int HOURS_IN_A_DAY = 24;
+
+        String hours = "";
+        String minutes = "";
+
+        int totalMinutes;
+        int totalHours;
+
+        hours = "" + time.charAt(0) + time.charAt(1);
+        minutes = "" + time.charAt(3) + time.charAt(4);
+
+        totalMinutes = ONE_HOUR_IN_MINUTES * Integer.parseInt(hours) + Integer.parseInt(minutes) + minutesToAdd;
+        totalHours = totalMinutes / ONE_HOUR_IN_MINUTES;
+        int newHours = totalHours % HOURS_IN_A_DAY;
+        int newMinutes = totalMinutes - (totalHours * ONE_HOUR_IN_MINUTES);
+
+        String formattedHours = "" + newHours;
+        String formattedMinutes = "" + newMinutes;
+
+        if(newHours < 10) {
+            formattedHours = "0" + formattedHours;
+        }
+        if(newMinutes < 10) {
+            formattedMinutes = "0" + formattedMinutes;
+        }
+
+        System.out.println(formattedHours + ":" + formattedMinutes);
     }
 }
